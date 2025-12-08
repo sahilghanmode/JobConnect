ac0e4b2d9e3ddca5f2f13d29dfbe1da338347483
sahilghanmode <sahilghanmode1903@gmail.com>
2025-12-08 19:14:41 +0530

    checking git cofigs

--- services/profile-service/src/main/java/com/jobconnect/profile/ProfileServiceApplication.java
+++ services/profile-service/src/main/java/com/jobconnect/profile/ProfileServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.jobconnect.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
